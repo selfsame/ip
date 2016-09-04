09a4f78552c5e49bc30d88673c7864116459be00
selfsame <josphe.parker.1@gmail.com>
2016-09-03 20:08:32 -0400

    initial experiment

---
+++ src/fs.clj
@@ -0,0 +1,31 @@
+(ns ip.fs
+  (:use ip.impl))
+
+(def ns-rx #"^\W*\(\W*ns[^\(\[]+")
+(def fn-rx #"(.+)\..*$")
+(def linkx #"Qm.{44}")
+
+(defn- forms [s] (read-string (str "(" s ")")))
+(defn- ns? [form] (first (map #(= 'ns %) form)))
+
+(defn- get-link [link] 
+  (in-dir ".ipfs"
+    (shell "ipfs" "get" link) 
+    (slurp link)))
+
+(defn fetch [sym s]
+  (when-let [txt (get-link (str s))]
+    (let [code (forms txt)
+          file (pathfile sym)
+          nsn  (last (re-find fn-rx file))]
+        (in-dir (pathdir sym)
+          (spit file 
+            (if (ns? (first code))
+                (clojure.string/replace txt ns-rx (str "(ns " nsn "\n  "))
+                (str "(ns " nsn ")\n" txt)))))))
+
+(defn pin [m] 
+  (in-dir ".ipfs" 
+    (spit "_" (prn-str m))
+    (->> (shell "ipfs" "add" "./_") 
+      :out str (re-find linkx))))
